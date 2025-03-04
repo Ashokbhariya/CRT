fac7fd1753ee20259478b014c512f368ead55fd5
Ashokbhariya <144993119+Ashokbhariya@users.noreply.github.com>
2025-03-04 23:31:07 +0530

    Add files via upload

---
+++ Assignment1/Q3.py
@@ -0,0 +1,4 @@
+#Take a number and use the += operator to increase its value by 10
+a=1
+a+=10
+print(a)
---
+++ Assignment1/Q4.py
@@ -0,0 +1,7 @@
+#Q2
+#Write a Python program to check if a given year is a leap year or not.
+a=int(input("enter year:"))
+if a%4==0:
+    print("This is a Leap Year")
+else:
+    print("This is not a leap year")
